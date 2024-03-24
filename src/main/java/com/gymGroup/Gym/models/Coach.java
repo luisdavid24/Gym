ac38f566599939fccd4d40e68046af2f4ac50cba
Luis David <david12.29@hotmail.com>
2024-03-24 18:07:59 -0500

    Feat(Back) Coach models is finished

--- src/main/java/com/gymGroup/Gym/models/Coach.java
+++ src/main/java/com/gymGroup/Gym/models/Coach.java
@@ -1,10 +1,33 @@
 package com.gymGroup.Gym.models;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "Porcine")
 public class Coach {
+
+
+    @Id
+    @Column(name = "Id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int Id;
 
+    @Column(name = "Name")
     private String Name;
+
+    @Column(name = "LastName")
     private String LastName;
+
+    @Column(name = "Role")
     private String Role;
 
 
