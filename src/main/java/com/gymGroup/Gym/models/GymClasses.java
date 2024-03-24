2104095614477cf8a397cbf07b5ca8385797150f
Luis David <david12.29@hotmail.com>
2024-03-24 18:24:22 -0500

    Feat(back) Gym clases had been finished

--- src/main/java/com/gymGroup/Gym/models/GymClasses.java
+++ src/main/java/com/gymGroup/Gym/models/GymClasses.java
@@ -1,4 +1,37 @@
 package com.gymGroup.Gym.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "Porcine")
 public class GymClasses {
+
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int ID;
+
+
+    @Column(name = "Name")
+    private String Name;
+
+    @Column(name = "Coach")
+    private List<Coach> Coach;
+
+    @Column(name = "Schedule")
+    private Date Schedule;
+
 }
