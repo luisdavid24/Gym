61c77741b0fc4b50f9aaba17868a03252c1f15d6
Luis David <david12.29@hotmail.com>
2024-03-24 18:45:24 -0500

    Feat(back) the Customer class was finished

--- src/main/java/com/gymGroup/Gym/models/Customer.java
+++ src/main/java/com/gymGroup/Gym/models/Customer.java
@@ -1,4 +1,43 @@
 package com.gymGroup.Gym.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "Customer")
 public class Customer {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int ID;
+
+    @Column(name = "Name")
+    private String Name;
+
+    @Column(name = "LastName")
+    private String LastName;
+
+    @Column(name = "Gender",nullable = true)
+    private String Gender;
+
+    @Column(name = "Phone")
+    private String Phone;
+
+    @Column(name = "NameEmergency",nullable = true)
+    private String NameEmergency;
+
+    @Column(name = "LastEmergency",nullable = true)
+    private String LastEmergency;
+
+    @Column(name = "PhoneEmergency",nullable = true)
+    private String PhoneEmergency;
+
 }
