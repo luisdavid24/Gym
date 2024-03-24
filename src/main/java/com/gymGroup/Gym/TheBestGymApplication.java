535798533aae9be15ecf8ecbe8f86d6b34849922
Luis David <david12.29@hotmail.com>
2024-03-24 17:01:58 -0500

    Initial commit

---
+++ src/main/java/com/gymGroup/Gym/TheBestGymApplication.java
@@ -0,0 +1,13 @@
+package com.gymGroup.Gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBestGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBestGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gymGroup/Gym/TheBestGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymGroup.Gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBestGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
