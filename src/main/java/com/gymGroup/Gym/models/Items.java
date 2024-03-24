895741bcc80b7b93c7a7b66b2715c679aefe9f89
Luis David <david12.29@hotmail.com>
2024-03-24 18:30:49 -0500

    Feat(back) Items class had created

--- src/main/java/com/gymGroup/Gym/models/Items.java
+++ src/main/java/com/gymGroup/Gym/models/Items.java
@@ -1,37 +1,28 @@
 package com.gymGroup.Gym.models;
 
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import java.util.Date;
-import java.util.List;
-
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 @Entity
-@Table(name = "GymClasses")
-public class GymClasses {
-
-
+@Table(name = "Items")
+public class Items {
     @Id
     @Column(name = "ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int ID;
 
+    @Column(name = "Description")
+    private String Description;
 
-    @Column(name = "Name")
-    private String Name;
-
-    @Column(name = "Coach")
-    private List<Coach> Coach;
-
-    @Column(name = "Schedule")
-    private Date Schedule;
+    @Column(name = "Amount")
+    private int Amount;
 
 }
